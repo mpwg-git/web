80fdcf5296532fb895e40c2ba616ee751d82bf27
pm@mpwg.com <peter@meineperfektewg.com>
2016-09-22 08:33:09 +0000

    (t) add new line fe_cookie.php

--- xsite/classes/fe_cookie.php
+++ xsite/classes/fe_cookie.php
@@ -7,60 +7,60 @@ class fe_cookie
 	const cookiename_fragen		= 'wsf_fragen';
 	const cookiename_cookiewarn = 'wsf_cookie_warning_seen';
 	const cookie_longterm_life_days = 1800;
-	
-	
+
+
 	public static function createLoginCookieForUserId($userId)
 	{
 		$userId = intval($userId);
 		if ($userId == 0) return false;
-		
+
 		// cookie exists? remove
 		if ($_COOKIE[self::cookiename] != "")
 		{
 			self::deleteLoginCookie();
 		}
-		
-		// create new hash and save 
+
+		// create new hash and save
 		$hash = self::genCookiehash();
 		self::setCookieByUserIdAndHash($userId, $hash);
 	}
-	
+
 	public static function deleteLoginCookie()
 	{
 		$hash = $_COOKIE[self::cookiename];
 		if ($hash == '') return false;
-		
+
 		// delete from db
 		$hash = dbx::escape($_COOKIE[self::cookiename]);
 		dbx::query("DELETE FROM ".self::cookietable." WHERE wz_COOKIE = '$hash' ");
-		
+
 		// delete from server
 		setcookie(self::cookiename, 'deleted', 1, '/');
-		
+
 		return true;
 	}
-	
+
 	public static function genCookiehash()
 	{
 		return md5( rand(0, 999999) . microtime(1) . 'WSF' . rand(0,99999) );
 	}
-	
+
 	public static function getUserIdByHash($hash)
 	{
 		if ($hash == '') return false;
-		$hash = dbx::escape($hash);	
+		$hash = dbx::escape($hash);
 		$usercookie = dbx::query("SELECT wz_USERID FROM " . self::cookietable . " WHERE wz_COOKIE = '$hash' AND wz_del = 'N' ");
 		if ($usercookie == false) return false;
 		return intval($usercookie['wz_USERID']);
 	}
-	
+
 	public static function setCookieByUserIdAndHash($userId, $hash)
 	{
 		$userId = intval($userId);
 		$hash   = trim($hash);
-		
+
 		if ($userId == 0 || $hash == '') return false;
-		
+
 		dbx::insert('wizard_auto_861', array(
 			'wz_created' 	=> 'NOW()',
 			'wz_USERID'		=> $userId,
@@ -68,20 +68,21 @@ class fe_cookie
 		));
 		setcookie(self::cookiename, $hash, time() + (86400 * self::cookie_lifetime_days), '/');
 	}
+
 	
 	public static function sc_checkCookie()
 	{
 		if ($_REQUEST['cms'] == 1) return true; // xGo Workaround
-		
+
 		// user kommt von startseite
 		if ($_REQUEST['url'] == '') {
 			if (isset($_COOKIE[self::cookiename]))
 			{
 				// try to find user via hash
 				$userId 	= self::getUserIdByHash($_COOKIE[self::cookiename]);
-				
+
 				if ($userId === false) return false;
-				
+
 				// log the user in
 				$feUserSessionKey 			 = xredaktor_feUser::getPrivateStatic('sessionName_FEUSER');
 				$userData 		  			 = dbx::query("SELECT * FROM wizard_auto_707 WHERE wz_id = $userId ");
@@ -92,7 +93,7 @@ class fe_cookie
 		}
 		return true;
 	}
-	
+
 	public static function sc_checkFragencookie()
 	{
 		if (!$_COOKIE[self::cookiename_fragen])
@@ -104,27 +105,27 @@ class fe_cookie
 
 		return false;
 	}
-	
+
 	public static function checkFragenSession()
 	{
 		@session_start();
-		
+
 		if(!isset($_SESSION['SHOWFRAGENCOOKIE']))
 		{
 			$_SESSION['SHOWFRAGENCOOKIE'] = false;
 			return true;
 		}
-				
+
 		return false;
 	}
-	
+
 	public static function sc_showCookieUsageWarning()
 	{
 		if (!$_COOKIE[self::cookiename_cookiewarn])
 		{
 			return true;
 		}
-		
+
 		return false;
 	}
 
@@ -132,14 +133,14 @@ class fe_cookie
 	{
 		setcookie(self::cookiename_cookiewarn, '1', time() + (86400 * self::cookie_longterm_life_days) , '/');
 	}
-	
-		
-	
+
+
+
 	/*
 	public static function updateCookieByHashAndData($hash, $data)
 	{
 		dbx::update(fe_user::cookietable, $data, array('wz_COOKIE' => $hash));
 	}
 	*/
-	
+
 }
